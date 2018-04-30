d1dd5f944cb8427c3bb7ef9a198d39a47c9c3e8e
songsd <none>
2018-04-30 10:04:29 +0800

    first add

---
+++ init.el
@@ -0,0 +1,71 @@
+
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/init/")
+
+(require 'init-packages)
+(require 'init-ui)
+
+
+
+
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+
+
+
+
+
+;; 关闭缩进 (第二天中被去除)
+;; (electric-indent-mode -1)
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+(global-hl-line-mode 1)
+
+
+
+
+
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+					; when Smex is auto-initialized on its first run.
+
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(load-theme 'monokai 1)
+
+;;格式化整个文件函数
+(defun indent-whole ()
+  (interactive)
+  (indent-region (point-min) (point-max))
+  (message "format successfully"))
+;;绑定到F3键
+(global-set-key [f3] 'indent-whole)
+
+
+(setq auto-save-default nil)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(require 'popwin)
+(popwin-mode 1)
+
+(setq auto-save-default nil)
+
+
---
+++ init/init-ui.el
@@ -0,0 +1,44 @@
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(package-selected-packages (quote (monokai-theme ## smex))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+(tool-bar-mode -1)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; 显示行号
+(global-linum-mode 1)
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq cursor-type 'bar)
+(setq-default cursor-type 'bar)
+
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+(set-face-attribute 'default nil :height 160)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+
+(provide 'init-ui)
