8a7308069011faa43f84569f62f2596e06945130
songsd <none>
2018-05-12 21:14:39 +0800

    add init-org

---
+++ init/init-org.el
@@ -0,0 +1,14 @@
+(require 'org)
+(setq org-src-fontify-natively t)
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/org"))
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/.emacs.d/gtd.org" "工作安排")
+	 "* TODO [#B] %?\n  %i\n"
+	 :empty-lines 1)))
+
+(global-set-key (kbd "C-c r") 'org-capture)
+
+
+(provide 'init-org)
